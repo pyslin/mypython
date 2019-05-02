b33d9aaeaa86a4d12e53acc3658090bf6b5b2207
pyslin <xingkemo@icloud.com>
2019-05-02 14:01:43 +0800

    改名

--- dict.py
+++
@@ -1,4 +0,0 @@
-d = {'Michael':95,'Tom':85,'Eric':100}
-print(d['Eric'])
-d['Micheal'] = 91
-print(d.get('Job'))
--- logging.py
+++
@@ -1,8 +0,0 @@
-import logging
-#logging.basicConfig(level=logging.INFO)
-logging.info("error")
-
-s = '0'
-n = int(s)
-logging.info('n = %d' % n)
-print(10 / n)
