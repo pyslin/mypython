d6e14a96152aea5bd5d14130f4de830a7dafe0e0
pyslin <xingkemo@icloud.com>
2019-05-08 13:02:24 +0800

    123

--- 1.py
+++ 1.py
@@ -5,3 +5,4 @@ conn = mysql.connector.connect(user='root', password='password', database='test'
 cursor = conn.cursor()
 # 创建user表:
 cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+123
